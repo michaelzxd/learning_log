883a966e239e4f6fd8ebb69abac9def57b60ea3c
Xiaodong <403533049@qq.com>
2020-06-18 22:47:17 +0800

    topic

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.text

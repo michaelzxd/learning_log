0b05c81ea5ca91f02d117beb37aeb6e4ad03547e
Xiaodong <403533049@qq.com>
2020-06-25 10:56:42 +0800

    edit entry completed

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Topic
+from .models import Topic, Entry
 from django.http import HttpResponseRedirect
 from django.urls import reverse 
 from .forms import TopicForm,EntryForm
@@ -47,8 +47,21 @@ def new_entry(request,topic_id):
 			new_entry.save()
 			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
 
-	context = {'topic':topic, 'form': form}
-	return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request,entry_id):
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+
+	if request.method != 'POST':
+		form = EntryForm(instance=entry)
+	else:
+		form = EntryForm(instance=entry,data = request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+	context = {'entry':entry,'topic':topic, 'form': form}
+	return render(request, 'learning_logs/edit_entry.html', context)
 
 
 
